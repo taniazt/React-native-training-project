a15b16652569245d5c650877436aa8ff38bdccb1
zhebil <yevhenii.bilyk@flexum.io>
2021-10-25 13:25:09 +0300

    add logger midleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,21 @@
 import {configureStore} from '@reduxjs/toolkit';
 import settingsReducer from './toggleSlice';
 
+const middleware = [];
+
+if (__DEV__) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, global-require
+  const reduxLogger = require('redux-logger');
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  middleware.push(reduxLogger.createLogger());
+}
+
 const store = configureStore({
   reducer: {
     settings: settingsReducer,
   },
+  middleware,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
